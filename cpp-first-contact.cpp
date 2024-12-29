717a32c642b4e1f28d926ad20fdd85b226b805e5
Allan Gilbert <allanrizza@gmail.com>
2024-12-29 04:06:58 -0300

    first commit

---
+++ cpp-first-contact.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h> //include every single function of c++
+
+using namespace std; //with this, i can use the classes and functions of the std library directly
+
+int main() {
+    string s;
+
+    getline(cin, s); //to read a whole line from the input
+
+    cout << s << "\n";
+}
