cb0bf2d0517a891399aee9b8892e75d6adbf7d33
Allan Gilbert <allanrizza@gmail.com>
2024-12-30 19:01:36 -0300

    define for and define square function bug

--- shortening-code.cpp
+++ shortening-code.cpp
@@ -7,16 +7,22 @@ typedef vector<pair<int, ld>> vi;
 
 #define PB push_back
 #define MP make_pair
-
+#define FOR_PAIR(v) for (pair<int, ld> p : v)
+#define SQ_BUG(a) a*a
+#define SQ(a) (a)*(a)
 
 int main() {
     vi vector;
     vector.PB(MP(1, 2.54237225));
     vector.PB(MP(2, 1.64534));
 
-    for(pair<int, ld> p : vector) {
+    FOR_PAIR(vector) {
         cout << p.first << ": " << p.second << "\n";
     }
+
+    cout << SQ_BUG(3+3) << endl; // 3+3*3+3
+
+    cout << SQ(3+3) << endl; // (3+3)*(3+3)
 
     return 0;
 }
