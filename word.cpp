583b3e2edbcd1a31702d72245b1319b1ff9333b1
Allan Gilbert <allanrizza@gmail.com>
2025-01-08 21:56:23 -0300

    word

---
+++ word.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string word;
+    set<char> l {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    set<char> u {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    int uq = 0;
+    int lq = 0;
+
+    cin >> word;
+
+    for (int i = 0; i < word.length(); i++)
+    {
+        if(l.find(word[i]) != l.end()) {
+            lq++;
+        } else {
+            uq++;
+        }
+    }
+
+    if(uq > lq) {
+        for (int i = 0; i < word.length(); i++){
+            word[i] = toupper(word[i]);
+        }
+    } else {
+        for (int i = 0; i < word.length(); i++){
+            word[i] = tolower(word[i]);
+        }
+    }
+    
+    cout << word;
+}
